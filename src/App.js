02643372fb26a75e5e3e4922e01ef52de120c942
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-21 17:54:12 +0200

    feat: add fetchData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_SITE_URL = process.env.REACT_APP_API_SITE_URL;
 
 function App() {
+  const [currentMeal, setCurrentMeal] = useState({});
+
+  const fetchData = async (endpoint) => {
+    const { data, error } = await axios.get(
+      `${API_SITE_URL + API_KEY}/search.php?s=${endpoint}`
+    );
+    if (error) {
+      return;
+    }
+    setCurrentMeal(data.meals[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        className="bg-blue-500 border rounded-xl p-5"
+        onClick={() => {
+          fetchData("Arrabiata");
+        }}
+      >
+        SHOW RECIPE
+      </button>
+      <div>{currentMeal.strMeal}</div>
     </div>
   );
 }
