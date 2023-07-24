5974884cc1cc4ed2731d8d1d407f8e4d91b5f2d8
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-24 16:24:21 +0200

    feat: add Tag component

---
+++ src/components/Tag.js
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+
+function Tag({ className, value, onClick }) {
+  const tagClasses = classNames(
+    " bg-green-200 p-1 rounded-xl cursor-pointer",
+    className
+  );
+
+  return (
+    <div className={tagClasses} onClick={onClick}>
+      {value}
+    </div>
+  );
+}
+
+export default Tag;
