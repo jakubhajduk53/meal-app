cfa5598a2ce4ca3661efdadfa5cf53a64ea85516
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-25 19:12:17 +0200

    feat: add CategoryItem component

---
+++ src/components/CategoryItem.js
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+
+function CategoryItem({ value, className, onClick }) {
+  const categoryItemClasses = classNames(
+    "text-lg hover:text-blue-400 cursor-pointer",
+    className
+  );
+
+  return (
+    <div onClick={onClick} className={categoryItemClasses}>
+      {value}
+    </div>
+  );
+}
+
+export default CategoryItem;
