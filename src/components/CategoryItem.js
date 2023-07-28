b9f86a4f180cdfa23a8d3b8b69855d783ede8b0f
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-28 21:00:49 +0200

    style: update categoryItem styles

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -2,8 +2,7 @@ import classNames from "classnames";
 
 function CategoryItem({ value, className, onClick, type }) {
   const categoryItemClasses = classNames(
-    "text-lg hover:text-blue-400",
-    type === "category" ? "cursor-pointer" : null,
+    "text-lg hover:text-blue-400 cursor-pointer",
     className
   );
 
