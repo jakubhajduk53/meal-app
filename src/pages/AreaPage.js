e4f2a33db51301d64ecc88fe71b4b2901d8864f1
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-29 18:39:55 +0200

    feat: update AreaPage

--- src/pages/AreaPage.js
+++ src/pages/AreaPage.js
@@ -1,55 +1,11 @@
-import { useEffect, useState } from "react";
-import AreaList from "../components/AreaList";
-import { createSelector } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAreaItems, fetchAreas, resetAreaItems } from "../store";
-
-const getAreas = (state) => state.areas.areasList;
-
-const selectAreas = createSelector([getAreas], (areasList) => areasList);
-
-const getAreaItems = (state) => state.areas.areaItems;
-
-const selectAreaItems = createSelector(
-  [getAreaItems],
-  (areaItems) => areaItems
-);
+import { Outlet } from "react-router";
 
 function AreaPage() {
-  const dispatch = useDispatch();
-
-  const [isSelected, setIsSelected] = useState(false);
-
-  const areasList = useSelector(selectAreas);
-
-  const areaItems = useSelector(selectAreaItems);
-
-  const handleClick = async (areaName) => {
-    setIsSelected(true);
-    dispatch(fetchAreaItems({ areaName }));
-  };
-
-  useEffect(() => {
-    dispatch(fetchAreas());
-    dispatch(resetAreaItems());
-  }, []);
-
   return (
     <div className="grid justify-items-center">
-      <p className="text-3xl mb-3">Areas</p>
+      <p className="text-3xl">Areas</p>
       <div>
-        {isSelected ? (
-          <AreaList
-            items={areaItems}
-            type="items"
-            handleClick={() => {
-              setIsSelected(false);
-              dispatch(resetAreaItems());
-            }}
-          />
-        ) : (
-          <AreaList items={areasList} type="area" handleClick={handleClick} />
-        )}
+        <Outlet />
       </div>
     </div>
   );
