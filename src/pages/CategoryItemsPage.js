cd185260dd2a1cf5f63646f44b1b4634b1a20755
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-29 18:39:39 +0200

    feat: add CategoryItemsPage

---
+++ src/pages/CategoryItemsPage.js
@@ -0,0 +1,45 @@
+import CategoryList from "../components/CategoryList";
+import { createSelector } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import Button from "../components/Button";
+import { useNavigate } from "react-router";
+
+const getCategoryItems = (state) => state.categories.categoryItems;
+
+const selectCategoryItems = createSelector(
+  [getCategoryItems],
+  (categoryItems) => categoryItems
+);
+
+const getCategoryName = (state) => state.categories.selectedCategory;
+
+const selectCategoryName = createSelector(
+  [getCategoryName],
+  (selectedCategory) => selectedCategory
+);
+
+function CategoryItemsPage() {
+  const navigate = useNavigate();
+
+  const categoryItems = useSelector(selectCategoryItems);
+
+  const categoryName = useSelector(selectCategoryName);
+
+  return (
+    <div className="grid justify-items-center">
+      <p className="text-xl italic">{categoryName ? categoryName : null}</p>
+      <div>
+        <CategoryList items={categoryItems} type="items" />
+      </div>
+      <Button
+        value="Go back"
+        onClick={() => {
+          navigate("../list");
+        }}
+        className="mt-3 mb-3"
+      />
+    </div>
+  );
+}
+
+export default CategoryItemsPage;
