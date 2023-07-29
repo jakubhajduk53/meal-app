db74f7191ee65ab7c2216fb0c603d16e7e69edc0
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-29 18:39:18 +0200

    feat: update CategoryPage

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,66 +1,11 @@
-import { useEffect, useState } from "react";
-import CategoryList from "../components/CategoryList";
-import { createSelector } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchCategories,
-  fetchCategoryItems,
-  resetCategoryItems,
-} from "../store/slices/categorySlice";
-
-const getCategories = (state) => state.categories.categoriesList;
-
-const selectCategories = createSelector(
-  [getCategories],
-  (categoriesList) => categoriesList
-);
-
-const getCategoryItems = (state) => state.categories.categoryItems;
-
-const selectCategoryItems = createSelector(
-  [getCategoryItems],
-  (categoryItems) => categoryItems
-);
+import { Outlet } from "react-router";
 
 function CategoryPage() {
-  const dispatch = useDispatch();
-
-  const [isSelected, setIsSelected] = useState(false);
-
-  const categoriesList = useSelector(selectCategories);
-
-  const categoryItems = useSelector(selectCategoryItems);
-
-  const handleClick = async (categoryName) => {
-    setIsSelected(true);
-    dispatch(fetchCategoryItems({ categoryName }));
-  };
-
-  useEffect(() => {
-    dispatch(fetchCategories());
-    dispatch(resetCategoryItems());
-  }, []);
-
   return (
     <div className="grid justify-items-center">
       <p className="text-3xl mb-3">Categories</p>
       <div>
-        {isSelected ? (
-          <CategoryList
-            items={categoryItems}
-            type="items"
-            handleClick={() => {
-              setIsSelected(false);
-              dispatch(resetCategoryItems());
-            }}
-          />
-        ) : (
-          <CategoryList
-            items={categoriesList}
-            type="category"
-            handleClick={handleClick}
-          />
-        )}
+        <Outlet />
       </div>
     </div>
   );
