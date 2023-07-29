d33574e6810e1df8d80215bc559266cd5328d4cf
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-29 17:32:17 +0200

    chore: remove type from CategoryItem

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 
-function CategoryItem({ value, className, onClick, type }) {
+function CategoryItem({ value, className, onClick }) {
   const categoryItemClasses = classNames(
     "text-lg hover:text-blue-400 cursor-pointer",
     className
